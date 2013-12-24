e9f2c2a4f2a39df87a671a05e5c0c64123a1959f
Gobelinus <silent.arora@gmail.com>
2013-12-24 17:55:28 +0530

    WIP - cover, intro, portfolio (wip)

---
+++ static/js/app.full.js
@@ -0,0 +1,11 @@
+/*
+ * App related code goes in here
+ */
+function appInit()
+{
+    //initialize app code here
+    $(document).ready(function() 
+    {
+    });
+}
+
---
+++ static/js/app.js
@@ -0,0 +1,1 @@
+function appInit(){$(document).ready(function(){})};
